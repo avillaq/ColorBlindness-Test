903cf048b1bb3baa466c7d60485c9385f5bc68de
avillaq <villafuertequispealex@gmail.com>
2025-02-28 11:42:30 -0500

    refactor: remove App component and CSS, implement routing with Layout and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HeroUIProvider } from '@heroui/react'
+import { RouterProvider } from './routes/RouterProvider'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HeroUIProvider>
-      <App />
+      <RouterProvider />
     </HeroUIProvider>
   </StrictMode>,
 )
