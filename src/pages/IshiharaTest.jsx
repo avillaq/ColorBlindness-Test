e5d17cd2a0648a454d19e2cdca2dc5854c651899
avillaq <villafuertequispealex@gmail.com>
2025-03-05 14:44:06 -0500

    feat: enhance Ishihara Test component with improved layout and styling

--- src/pages/IshiharaTest.jsx
+++ src/pages/IshiharaTest.jsx
@@ -1,5 +1,15 @@
+import "../styles/pages/IshiharaTest.css";
+
 export const IshiharaTest = () => {
   return (
-    <div>IshiharaTest</div>
+    <div className="ishihara-test-container">
+      <div className="content-text">
+        <h2>Ishihara Test</h2>
+        <p>Use the Ishihara Test to detect red-green color deficiencies. Quick and reliable assessment for your color vision health.</p>
+      </div>
+      <div className="content-ishihara-test">
+
+      </div>
+    </div>
   )
 }
