21c0984e6e77f073af6bd9415026860e6618529b
avillaq <villafuertequispealex@gmail.com>
2025-03-04 23:18:31 -0500

    feat: enhance layout and structure of Tests page for better readability

--- src/pages/Tests.jsx
+++ src/pages/Tests.jsx
@@ -10,88 +10,89 @@ import kidsImage from "../assets/kids-test.webp"
 
 export const Tests = () => {
   return (
-    <div className="tests-container">
-      <p>We use below tests to check color vision deficiency.</p>
-      <section>
-        <div className="content-tests">
-          <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
-            <CardHeader className="flex flex-col gap-3">
-              <Image
-                alt="Ishihara Test logo"
-                height={80}
-                radius="full"
-                src={ishiharaImage}
-                width={80}
-              />
-              <h3>Ishihara Test</h3>
-            </CardHeader>
-          </Card>
-          <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
-            <CardHeader className="flex flex-col gap-3">
-              <Image
-                alt="Farnsworth d-15 logo"
-                height={80}
-                radius="full"
-                src={farnsworthD15Image}
-                width={80}
-              />
-              <h3>Farnsworth D-15</h3>
-            </CardHeader>
-          </Card>
+    <div className="tests-container content-section">
+      <div className="content-text">
+        <p>We use below tests to check color vision deficiency.</p>
 
-          <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
-            <CardHeader className="flex flex-col gap-3">
-              <Image
-                alt="Cambridge Test logo"
-                height={80}
-                radius="full"
-                src={cambridgeImage}
-                width={80}
-              />
-              <h3>Cambridge Colour Test</h3>
-            </CardHeader>
-          </Card>
+      </div>
+      <div className="content-tests">
+        <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
+          <CardHeader className="flex flex-col gap-3">
+            <Image
+              alt="Ishihara Test logo"
+              height={80}
+              radius="full"
+              src={ishiharaImage}
+              width={80}
+            />
+            <h3>Ishihara Test</h3>
+          </CardHeader>
+        </Card>
+        <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
+          <CardHeader className="flex flex-col gap-3">
+            <Image
+              alt="Farnsworth d-15 logo"
+              height={80}
+              radius="full"
+              src={farnsworthD15Image}
+              width={80}
+            />
+            <h3>Farnsworth D-15</h3>
+          </CardHeader>
+        </Card>
 
-          <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
-            <CardHeader className="flex flex-col gap-3">
-              <Image
-                alt="Anomaloscope Test logo"
-                height={80}
-                radius="full"
-                src={anomaloscopeImage}
-                width={80}
-              />
-              <h3>Anomaloscope Test</h3>
-            </CardHeader>
-          </Card>
+        <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
+          <CardHeader className="flex flex-col gap-3">
+            <Image
+              alt="Cambridge Test logo"
+              height={80}
+              radius="full"
+              src={cambridgeImage}
+              width={80}
+            />
+            <h3>Cambridge Colour Test</h3>
+          </CardHeader>
+        </Card>
 
-          <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
-            <CardHeader className="flex flex-col gap-3">
-              <Image
-                alt="Farnsworth Lantern logo"
-                height={80}
-                radius="full"
-                src={farnsworthLanternImage}
-                width={80}
-              />
-              <h3>Farnsworth Lantern Test</h3>
-            </CardHeader>
-          </Card>
+        <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
+          <CardHeader className="flex flex-col gap-3">
+            <Image
+              alt="Anomaloscope Test logo"
+              height={80}
+              radius="full"
+              src={anomaloscopeImage}
+              width={80}
+            />
+            <h3>Anomaloscope Test</h3>
+          </CardHeader>
+        </Card>
 
-          <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
-            <CardHeader className="flex flex-col gap-3">
-              <Image
-                alt="Kids Test logo"
-                height={80}
-                radius="full"
-                src={kidsImage}
-                width={80}
-              />
-              <h3>Kids ColorBlindness Test</h3>
-            </CardHeader>
-          </Card>
-        </div>
-      </section>
+        <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
+          <CardHeader className="flex flex-col gap-3">
+            <Image
+              alt="Farnsworth Lantern logo"
+              height={80}
+              radius="full"
+              src={farnsworthLanternImage}
+              width={80}
+            />
+            <h3>Farnsworth Lantern Test</h3>
+          </CardHeader>
+        </Card>
+
+        <Card className="test-card" classNames={{ header: "p-3.5", body: "p-3.5" }} >
+          <CardHeader className="flex flex-col gap-3">
+            <Image
+              alt="Kids Test logo"
+              height={80}
+              radius="full"
+              src={kidsImage}
+              width={80}
+            />
+            <h3>Kids ColorBlindness Test</h3>
+          </CardHeader>
+        </Card>
+      </div>
     </div>
   )
 }
