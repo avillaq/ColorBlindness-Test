679ca24de54cb37585bca0f3e5198a28573ecf6b
avillaq <villafuertequispealex@gmail.com>
2025-03-05 00:30:05 -0500

    feat: add routes for new test components including Ishihara, Farnsworth D15, Cambridge, Farnsworth Lantern, Anomaloscope, and Kids tests

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,12 @@
 import { Home } from '../pages/Home';
 import { Tests } from '../pages/Tests';
 import { NotFound } from '../pages/NotFound';
+import { IshiharaTest } from '../pages/IshiharaTest';
+import { FarnsworthD15Test } from '../pages/FarnsworthD15Test';
+import { CambridgeTest } from '../pages/CambridgeTest';
+import { FarnsworthLanternTest } from '../pages/FarnsworthLanternTest';
+import { AnomaloscopeTest } from '../pages/AnomaloscopeTest';
+import { KidsTest } from '../pages/KidsTest';
 
 export const routes = [
   {
@@ -8,11 +14,35 @@ export const routes = [
     element: Home,
   },
   {
+    path: '/tests',
+    element: Tests,
+  },
+  {
+    path: '/tests/ishihara-test',
+    element: IshiharaTest,
+  },
+  {
+    path: '/tests/farnsworth-d-15-test',
+    element: FarnsworthD15Test,
+  },
+  {
+    path: '/tests/cambridge-test',
+    element: CambridgeTest,
+  },
+  {
+    path: '/tests/farnsworth-lantern-test',
+    element: FarnsworthLanternTest,
+  },
+  {
+    path: '/tests/anomaloscope-test',
+    element: AnomaloscopeTest,
+  },
+  {
+    path: '/tests/kids-colorblindness-test',
+    element: KidsTest,
+  },
+  {
     path: '*',
     element: NotFound,
   },
-  {
-    path: '/tests',
-    element: Tests,
-  }
 ];
