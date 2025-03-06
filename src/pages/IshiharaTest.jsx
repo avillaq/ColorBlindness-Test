c82aa6bfeb210ceda7fb4711012ad005b92063ec
avillaq <villafuertequispealex@gmail.com>
2025-03-06 02:48:25 -0500

    feat: adjust layout and styling in Ishihara Test for improved responsiveness and user experience

--- src/pages/IshiharaTest.jsx
+++ src/pages/IshiharaTest.jsx
@@ -9,12 +9,12 @@ export const IshiharaTest = () => {
   const [showTest, setShowTest] = useState(false);
 
   return (
-    <div className="ishihara-test-container">
+    <div className={`ishihara-test-container ${showTest ? "gap-4" : "gap-12"}`}>
       <div className="content-text">
         <h2>Ishihara Test</h2>
         {!showTest && <p>Use the Ishihara Test to detect red-green color deficiencies. Quick and reliable assessment for your color vision health.</p>}
       </div>
-      <div className="content-ishihara-test">
+      <div className={`content-ishihara-test ${showTest ? "text-right" : "text-center"}`}>
         {!showTest && <Button color="primary" onPress={() => setShowTest(true)}>Start Test</Button>}
         {showTest &&
           <>
