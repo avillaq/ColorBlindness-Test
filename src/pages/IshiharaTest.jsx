6f9c90e30fb10513653ff98bc6f6bf8f7203bbaa
avillaq <villafuertequispealex@gmail.com>
2025-03-05 21:06:23 -0500

    feat: enhance Ishihara Test layout with new card component and styling

--- src/pages/IshiharaTest.jsx
+++ src/pages/IshiharaTest.jsx
@@ -1,3 +1,4 @@
+import { Card, CardBody } from "@heroui/card";
 import "../styles/pages/IshiharaTest.css";
 
 export const IshiharaTest = () => {
@@ -8,7 +9,16 @@ export const IshiharaTest = () => {
         <p>Use the Ishihara Test to detect red-green color deficiencies. Quick and reliable assessment for your color vision health.</p>
       </div>
       <div className="content-ishihara-test">
-
+        <Card>
+          <CardBody className="flex flex-row">
+            <div className="ishihara-test-plates">
+              image goes here
+            </div>
+            <div className="ishihara-test-controls">
+              controls go here
+            </div>
+          </CardBody>
+        </Card>
       </div>
     </div>
   )
