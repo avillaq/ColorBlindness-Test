a482bb939789a7d112f505aecc2298b19b38ed2e
avillaq <villafuertequispealex@gmail.com>
2025-03-06 02:21:46 -0500

    feat: add interactive functionality to Ishihara Test with start button and image display

--- src/pages/IshiharaTest.jsx
+++ src/pages/IshiharaTest.jsx
@@ -1,24 +1,43 @@
+import { useState } from "react";
 import { Card, CardBody } from "@heroui/card";
+import { Image } from "@heroui/image";
+import { Button } from "@heroui/button";
 import "../styles/pages/IshiharaTest.css";
+import ishiharaImage from "../assets/ishihara-test.webp"
 
 export const IshiharaTest = () => {
+  const [showTest, setShowTest] = useState(false);
+
   return (
     <div className="ishihara-test-container">
       <div className="content-text">
         <h2>Ishihara Test</h2>
-        <p>Use the Ishihara Test to detect red-green color deficiencies. Quick and reliable assessment for your color vision health.</p>
+        {!showTest && <p>Use the Ishihara Test to detect red-green color deficiencies. Quick and reliable assessment for your color vision health.</p>}
       </div>
       <div className="content-ishihara-test">
-        <Card className="h-[610px] md:h-[428px]">
-          <CardBody className="cardbody-test">
-            <div className="ishihara-test-plates">
-              image goes here
-            </div>
-            <div className="ishihara-test-controls">
-              controls go here
-            </div>
-          </CardBody>
-        </Card>
+        {!showTest && <Button color="primary" onPress={() => setShowTest(true)}>Start Test</Button>}
+        {showTest &&
+          <>
+            <Button size="sm" isIconOnly color="primary" variant="shadow" onPress={() => setShowTest(false)} >
+              <box-icon name='x'></box-icon>
+            </Button>
+            <Card className="h-[610px] md:h-[428px]">
+              <CardBody className="cardbody-test">
+                <div className="ishihara-test-plates">
+                  <Image
+                    alt="Ishihara Test Plates"
+                    src={ishiharaImage}
+                    width={400}
+                  />
+                </div>
+                <div className="ishihara-test-controls">
+                  controls go here
+                </div>
+              </CardBody>
+            </Card>
+          </>
+        }
+
       </div>
     </div>
   )
