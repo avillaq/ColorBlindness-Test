4cd94a04decc6042c1e28e73af3706ed24fd27b1
avillaq <villafuertequispealex@gmail.com>
2025-03-07 13:06:49 -0500

    feat: add Ishihara plates data for color vision testing

---
+++ src/utils/test-plates.js
@@ -0,0 +1,17 @@
+export const ishiharaPlates = [
+  { id: 1, imageUrl: "plate1.webp", normalAnswer: "12", deficientAnswer: "12", type: "control" },
+  { id: 2, imageUrl: "plate2.webp", normalAnswer: "8", deficientAnswer: "3", type: "test" },
+  { id: 3, imageUrl: "plate3.webp", normalAnswer: "5", deficientAnswer: "2", type: "test" },
+  { id: 4, imageUrl: "plate4.webp", normalAnswer: "29", deficientAnswer: "70", type: "test" },
+  { id: 5, imageUrl: "plate5.webp", normalAnswer: "74", deficientAnswer: "21", type: "test" },
+  { id: 6, imageUrl: "plate6.webp", normalAnswer: "7", deficientAnswer: "X", type: "test" },
+  { id: 7, imageUrl: "plate7.webp", normalAnswer: "45", deficientAnswer: "X", type: "test" },
+  { id: 8, imageUrl: "plate8.webp", normalAnswer: "2", deficientAnswer: "X", type: "test" },
+  { id: 9, imageUrl: "plate9.webp", normalAnswer: "X", deficientAnswer: "2", type: "test" },
+  { id: 10, imageUrl: "plate10.webp", normalAnswer: "16", deficientAnswer: "X", type: "test" },
+  { id: 11, imageUrl: "plate11.webp", normalAnswer: "traceable", deficientAnswer: "X", type: "test" },
+
+  { id: 12, imageUrl: "plate12.webp", normalAnswer: "35", protanAnswer: "5", deutanAnswer: "3", type: "diagnostic" },
+  { id: 13, imageUrl: "plate13.webp", normalAnswer: "96", protanAnswer: "6", deutanAnswer: "9", type: "diagnostic" },
+  { id: 14, imageUrl: "plate14.webp", normalAnswer: "purple,red", protanAnswer: "purple", deutanAnswer: "red", type: "diagnostic" },
+];
