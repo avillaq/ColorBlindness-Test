9bc021adb72bc0b5e9ebe153428c7d54dab4f07f
avillaq <villafuertequispealex@gmail.com>
2025-03-07 14:15:44 -0500

    feat: update image paths for Ishihara plates in utility file

--- src/utils/ishihara-test.js
+++ src/utils/ishihara-test.js
@@ -1,19 +1,21 @@
-export const ishiharaPlates = [
-  { id: 1, imageUrl: "plate1.webp", normalAnswer: "12", deficientAnswer: "12", type: "control" },
-  { id: 2, imageUrl: "plate2.webp", normalAnswer: "8", deficientAnswer: "3", type: "test" },
-  { id: 3, imageUrl: "plate3.webp", normalAnswer: "5", deficientAnswer: "2", type: "test" },
-  { id: 4, imageUrl: "plate4.webp", normalAnswer: "29", deficientAnswer: "70", type: "test" },
-  { id: 5, imageUrl: "plate5.webp", normalAnswer: "74", deficientAnswer: "21", type: "test" },
-  { id: 6, imageUrl: "plate6.webp", normalAnswer: "7", deficientAnswer: "X", type: "test" },
-  { id: 7, imageUrl: "plate7.webp", normalAnswer: "45", deficientAnswer: "X", type: "test" },
-  { id: 8, imageUrl: "plate8.webp", normalAnswer: "6", deficientAnswer: "X", type: "test" },
-  { id: 9, imageUrl: "plate9.webp", normalAnswer: "X", deficientAnswer: "45", type: "test" },
-  { id: 10, imageUrl: "plate10.webp", normalAnswer: "16", deficientAnswer: "X", type: "test" },
-  { id: 11, imageUrl: "plate11.webp", normalAnswer: "traceable", deficientAnswer: "X", type: "test" },
+// Source: https://web.stanford.edu/group/vista/wikiupload/0/0a/Ishihara.14.Plate.Instructions.pdf
 
-  { id: 12, imageUrl: "plate12.webp", normalAnswer: "26", protanAnswer: "6", deutanAnswer: "2", type: "diagnostic" },
-  { id: 13, imageUrl: "plate13.webp", normalAnswer: "42", protanAnswer: "2", deutanAnswer: "4", type: "diagnostic" },
-  { id: 14, imageUrl: "plate14.webp", normalAnswer: "purple,red", protanAnswer: "purple", deutanAnswer: "red", type: "diagnostic" },
+export const ishiharaPlates = [
+  { id: 1, imageUrl: "../assets/ishihara-plates/plate1.webp", normalAnswer: "12", deficientAnswer: "12", type: "control" },
+  { id: 2, imageUrl: "../assets/ishihara-plates/plate2.webp", normalAnswer: "8", deficientAnswer: "3", type: "test" },
+  { id: 3, imageUrl: "../assets/ishihara-plates/plate3.webp", normalAnswer: "5", deficientAnswer: "2", type: "test" },
+  { id: 4, imageUrl: "../assets/ishihara-plates/plate4.webp", normalAnswer: "29", deficientAnswer: "70", type: "test" },
+  { id: 5, imageUrl: "../assets/ishihara-plates/plate5.webp", normalAnswer: "74", deficientAnswer: "21", type: "test" },
+  { id: 6, imageUrl: "../assets/ishihara-plates/plate6.webp", normalAnswer: "7", deficientAnswer: "X", type: "test" },
+  { id: 7, imageUrl: "../assets/ishihara-plates/plate7.webp", normalAnswer: "45", deficientAnswer: "X", type: "test" },
+  { id: 8, imageUrl: "../assets/ishihara-plates/plate8.webp", normalAnswer: "6", deficientAnswer: "X", type: "test" },
+  { id: 9, imageUrl: "../assets/ishihara-plates/plate9.webp", normalAnswer: "X", deficientAnswer: "45", type: "test" },
+  { id: 10, imageUrl: "../assets/ishihara-plates/plate10.webp", normalAnswer: "16", deficientAnswer: "X", type: "test" },
+  { id: 11, imageUrl: "../assets/ishihara-plates/plate11.webp", normalAnswer: "traceable", deficientAnswer: "X", type: "test" },
+
+  { id: 12, imageUrl: "../assets/ishihara-plates/plate12.webp", normalAnswer: "26", protanAnswer: "6", deutanAnswer: "2", type: "diagnostic" },
+  { id: 13, imageUrl: "../assets/ishihara-plates/plate13.webp", normalAnswer: "42", protanAnswer: "2", deutanAnswer: "4", type: "diagnostic" },
+  { id: 14, imageUrl: "../assets/ishihara-plates/plate14.webp", normalAnswer: "purple,red", protanAnswer: "purple", deutanAnswer: "red", type: "diagnostic" },
 ];
 
 export const evaluateIshiharaResults = (answers, plates) => {
