ca861335473dd22b77b26a1c2b3d5607747400aa
avillaq <villafuertequispealex@gmail.com>
2025-03-07 13:18:40 -0500

    feat: update normal and deficient answers for Ishihara plates in test data

--- src/utils/test-plates.js
+++ src/utils/test-plates.js
@@ -6,12 +6,12 @@ export const ishiharaPlates = [
   { id: 5, imageUrl: "plate5.webp", normalAnswer: "74", deficientAnswer: "21", type: "test" },
   { id: 6, imageUrl: "plate6.webp", normalAnswer: "7", deficientAnswer: "X", type: "test" },
   { id: 7, imageUrl: "plate7.webp", normalAnswer: "45", deficientAnswer: "X", type: "test" },
-  { id: 8, imageUrl: "plate8.webp", normalAnswer: "2", deficientAnswer: "X", type: "test" },
-  { id: 9, imageUrl: "plate9.webp", normalAnswer: "X", deficientAnswer: "2", type: "test" },
+  { id: 8, imageUrl: "plate8.webp", normalAnswer: "6", deficientAnswer: "X", type: "test" },
+  { id: 9, imageUrl: "plate9.webp", normalAnswer: "X", deficientAnswer: "45", type: "test" },
   { id: 10, imageUrl: "plate10.webp", normalAnswer: "16", deficientAnswer: "X", type: "test" },
   { id: 11, imageUrl: "plate11.webp", normalAnswer: "traceable", deficientAnswer: "X", type: "test" },
 
-  { id: 12, imageUrl: "plate12.webp", normalAnswer: "35", protanAnswer: "5", deutanAnswer: "3", type: "diagnostic" },
-  { id: 13, imageUrl: "plate13.webp", normalAnswer: "96", protanAnswer: "6", deutanAnswer: "9", type: "diagnostic" },
+  { id: 12, imageUrl: "plate12.webp", normalAnswer: "26", protanAnswer: "6", deutanAnswer: "2", type: "diagnostic" },
+  { id: 13, imageUrl: "plate13.webp", normalAnswer: "42", protanAnswer: "2", deutanAnswer: "4", type: "diagnostic" },
   { id: 14, imageUrl: "plate14.webp", normalAnswer: "purple,red", protanAnswer: "purple", deutanAnswer: "red", type: "diagnostic" },
 ];
