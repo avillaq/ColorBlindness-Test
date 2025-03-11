d53532e9277935d6f3a28110f305772910345501
avillaq <villafuertequispealex@gmail.com>
2025-03-11 00:44:14 -0500

    chore: integrate flowbite-react plugin into Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 const { heroui } = require("@heroui/react");
-
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [heroui()]
+  plugins: [heroui(), flowbite.plugin(),]
 }
 
