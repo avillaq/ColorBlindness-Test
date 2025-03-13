b608a35a371d2cd49e8036b8a588de09d8e51bb0
avillaq <villafuertequispealex@gmail.com>
2025-03-13 13:58:12 -0500

    feat: add evaluateCambridgeResults function for diagnosing vision results

---
+++ src/utils/cambridge-test.js
@@ -0,0 +1,9 @@
+export const evaluateCambridgeResults = (results) => {
+  const diagnosis = {
+    type: "normal",
+    severity: "normal vision",
+    ellipseRatio: null
+  };
+
+  return diagnosis;
+};
