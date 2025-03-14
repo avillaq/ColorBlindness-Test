20ed976851d8b3420f0323f1b9e4ecec2707874a
avillaq <villafuertequispealex@gmail.com>
2025-03-13 23:09:23 -0500

    feat: update CambridgeTest plate type from control to test

--- src/utils/cambridge-test.js
+++ src/utils/cambridge-test.js
@@ -1,5 +1,5 @@
 export const cambridgePlates = [
-  { id: 1, imageUrl: "/src/assets/cambridge/cambridge-plates/plate1.webp", normalAnswer: "left", type: "control" },
+  { id: 1, imageUrl: "/src/assets/cambridge/cambridge-plates/plate1.webp", normalAnswer: "left", type: "test" },
   { id: 2, imageUrl: "/src/assets/cambridge/cambridge-plates/plate2.webp", normalAnswer: "right", type: "test" },
   { id: 3, imageUrl: "/src/assets/cambridge/cambridge-plates/plate3.webp", normalAnswer: "bottom", type: "test" },
   { id: 4, imageUrl: "/src/assets/cambridge/cambridge-plates/plate4.webp", normalAnswer: "top", type: "test" },
