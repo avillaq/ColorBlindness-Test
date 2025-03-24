e9482b03215cb87cf0cde3b5919c54c56a9d5cf6
avillaq <villafuertequispealex@gmail.com>
2025-03-23 23:50:28 -0500

    feat: replace Card components with Accordion for FAQ section

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,4 +1,4 @@
-import { Card, CardBody } from "@heroui/card"
+import {Accordion, AccordionItem} from "@heroui/accordion";
 import "../styles/pages/FAQ.css"
 
 export const FAQ = () => {
@@ -11,28 +11,17 @@ export const FAQ = () => {
         </div>
       </div>
       <div className="faq-content">
-        <Card className="sm:w-[420px] sm:h-[380px]"> 
-          <CardBody>
-            <div className="flex flex-col gap-4 p-4">
-              <h3>Our Story</h3>
-              <p>
-                ColorVision was developed to make professional color vision testing accessible to everyone. We offer scientifically validated tests including the Ishihara Test, Farnsworth D15, Cambridge Color Test, Farnsworth Lantern Test and Anomaloscope Test.<br />
-                Our platform is designed for both individuals concerned faq their color vision and professionals who require regular testing for their work.
-              </p>
-            </div>
-          </CardBody>
-        </Card>
-        <Card className="sm:w-[420px] sm:h-[380px]"> 
-          <CardBody>
-            <div className="flex flex-col gap-4 p-4">
-              <h3>Our Mission</h3>
-              <p>
-                Our mission is to provide reliable color vision testing that's both accessible and scientifically accurate. We focus on empowering users with detailed insights faq their color vision capabilities, whether for professional requirements or personal health monitoring. <br />
-                Through early detection and regular assessment, we help users maintain their quality of life and meet their professional standards.
-              </p>
-            </div>
-          </CardBody>
-        </Card>
+        <Accordion variant="bordered">
+          <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
+            <p>Hola</p>
+          </AccordionItem>
+          <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
+            <p>Hola</p>
+          </AccordionItem>
+          <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
+            <p>Hola</p>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
   )
