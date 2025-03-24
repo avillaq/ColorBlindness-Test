523cb8f8a1cb214ee0e053115a43cc7044287766
avillaq <villafuertequispealex@gmail.com>
2025-03-24 01:10:49 -0500

    feat: enhance Contact page with detailed information and improved layout

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,21 @@
+import "../styles/pages/Contact.css"
+
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact-container">
+      <div className="contact-hero">
+        <div className="content-text">
+          <h2>Contact Us</h2>
+          <p>Have questions or feedback? Reach out to the ColorVision team for assistance with our online vision tests.</p>
+        </div>
+      </div>
+      <div className="contact-content">
+        <div>
+          <p>We invite you to share your thoughts and ideas with us at ColorVision. Your feedback is crucial in enhancing our color vision testing tools and overall user experience. Whether you have found a feature that excites you, identified an area for improvement, or have a new idea, we’re eager to hear from you.</p>
+          <p>Your contributions play a key role in our journey toward continual improvement, helping us refine our services and innovate to serve you better.</p>
+          <p>Rest assured, our team thoroughly reviews every piece of feedback to ensure we exceed your expectations and enhance your experience on our site. With your input, we can make ColorVision an even more engaging and effective platform for everyone.</p>
+        </div>
+      </div>
+    </div>
   )
 }
