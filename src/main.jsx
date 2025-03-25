fb8e0b8b9f5e0b428993fd417f33cdf7d30c9462
avillaq <villafuertequispealex@gmail.com>
2025-03-24 23:21:50 -0500

    feat: add ToastProvider to main.jsx for enhanced notification support

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HeroUIProvider } from '@heroui/react'
+import { ToastProvider } from "@heroui/toast";
 import { RouterProvider } from './routes/RouterProvider'
 import './index.css'
 import 'boxicons';
@@ -8,6 +9,7 @@ import 'boxicons';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HeroUIProvider>
+      <ToastProvider />
       <RouterProvider />
     </HeroUIProvider>
   </StrictMode>,
