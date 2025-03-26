8c98f47a012527d61c0cd5971daec26aa88785e0
avillaq <villafuertequispealex@gmail.com>
2025-03-26 16:49:41 -0500

    feat: add site configuration for ColorVision platform

---
+++ config/siteConfig.js
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  name: "ColorVision",
+  title: "ColorVision - Professional Color Blindness Testing",
+  description: "Professional color blindness testing platform with accurate and educational resources",
+  keywords: [
+    "color blindness",
+    "color vision test",
+    "ishihara test",
+    "farnsworth d15",
+    "farnsworth lantern",
+    "cambridge color test",
+    "anomaloscope test"
+  ],
+  url: "https://colorvision-test.com",
+  author: "AlexanderVQ",
+  social: {
+    github: "https://github.com/avillaq"
+  }
+};
