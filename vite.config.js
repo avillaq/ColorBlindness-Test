3aca2d061c5389fa9066519d64fc4c2282ff73c6
avillaq <villafuertequispealex@gmail.com>
2025-03-26 16:58:10 -0500

    feat: add environment variables for site configuration in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { siteConfig } from "./src/config/site";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.VITE_APP_TITLE": JSON.stringify(siteConfig.title),
+    "process.env.VITE_APP_DESCRIPTION": JSON.stringify(siteConfig.description),
+    "process.env.VITE_APP_KEYWORDS": JSON.stringify(siteConfig.keywords.join(", ")),
+    "process.env.VITE_APP_AUTHOR": JSON.stringify(siteConfig.author),
+  }
+});
