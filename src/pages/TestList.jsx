0c236c4bb0e5b02c671cabecdebf009d032c09f7
avillaq <villafuertequispealex@gmail.com>
2025-03-26 23:06:18 -0500

    feat: replace useNavigate with Link component for navigation in TestList

--- src/pages/TestList.jsx
+++ src/pages/TestList.jsx
@@ -1,6 +1,6 @@
 import { Card, CardHeader } from "@heroui/card";
 import { Image } from "@heroui/image";
-import { useNavigate } from 'react-router-dom';
+import { Link } from "@heroui/link";
 import "../styles/pages/TestList.css"
 import anomaloscopeImage from "../assets/anomaloscope-test.webp"
 import cambridgeImage from "../assets/cambridge-test.webp"
@@ -9,7 +9,6 @@ import farnsworthLanternImage from "../assets/farnsworth-lantern-test.webp"
 import ishiharaImage from "../assets/ishihara-test.webp"
 
 export const TestList = () => {
-  const navigate = useNavigate();
   return (
     <div className="test-list-container">
       <div className="content-text">
@@ -20,7 +19,9 @@ export const TestList = () => {
           isPressable
           className="test-card"
           classNames={{ base: "w-56 h-48 justify-center", header: "px-3.5" }}
-          onPress={() => navigate("/tests/ishihara-test")} >
+          as={Link}
+          href="/tests/ishihara-test"
+        >
           <CardHeader className="flex flex-col gap-3">
             <Image
               alt="Ishihara Test logo"
@@ -36,7 +37,9 @@ export const TestList = () => {
           isPressable
           className="test-card"
           classNames={{ base: "w-56 h-48 justify-center", header: "px-3.5" }}
-          onPress={() => navigate("/tests/farnsworth-d-15-test")} >
+          as={Link}
+          href="/tests/farnsworth-d-15-test"
+        >
           <CardHeader className="flex flex-col gap-3">
             <Image
               alt="Farnsworth d-15 logo"
@@ -53,7 +56,9 @@ export const TestList = () => {
           isPressable
           className="test-card"
           classNames={{ base: "w-56 h-48 justify-center", header: "px-3.5" }}
-          onPress={() => navigate("/tests/cambridge-test")} >
+          as={Link}
+          href="/tests/cambridge-test"
+        >
           <CardHeader className="flex flex-col gap-3">
             <Image
               alt="Cambridge Test logo"
@@ -70,7 +75,9 @@ export const TestList = () => {
           isPressable
           className="test-card"
           classNames={{ base: "w-56 h-48 justify-center", header: "px-3.5" }}
-          onPress={() => navigate("/tests/anomaloscope-test")} >
+          as={Link}
+          href="/tests/anomaloscope-test"
+        >
           <CardHeader className="flex flex-col gap-3">
             <Image
               alt="Anomaloscope Test logo"
@@ -87,7 +94,9 @@ export const TestList = () => {
           isPressable
           className="test-card"
           classNames={{ base: "w-56 h-48 justify-center", header: "px-3.5" }}
-          onPress={() => navigate("/tests/farnsworth-lantern-test")} >
+          as={Link}
+          href="/tests/farnsworth-lantern-test"
+        >
           <CardHeader className="flex flex-col gap-3">
             <Image
               alt="Farnsworth Lantern logo"
