434a3b145700eb6f8eafe3b821698f1f056c5d51
avillaq <villafuertequispealex@gmail.com>
2025-03-26 22:16:55 -0500

    feat: simplify main rendering by removing unnecessary providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HeroUIProvider } from '@heroui/react'
-import { ToastProvider } from "@heroui/toast";
 import { RouterProvider } from './routes/RouterProvider'
 import './index.css'
 import 'boxicons';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HeroUIProvider>
-      <ToastProvider />
-      <RouterProvider />
-    </HeroUIProvider>
+    <RouterProvider />
   </StrictMode>,
 )
