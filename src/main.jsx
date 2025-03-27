5d2c1f9b1beaa8e0b22893facf59d5feca730d0d
avillaq <villafuertequispealex@gmail.com>
2025-03-27 17:57:33 -0500

    chore: remove unused boxicons import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from './routes/RouterProvider'
 import './index.css'
-import 'boxicons';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
