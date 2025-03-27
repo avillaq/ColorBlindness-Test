140501d03673e87435599c9f34552375ec5c8216
avillaq <villafuertequispealex@gmail.com>
2025-03-26 22:17:06 -0500

    feat: integrate HeroUIProvider and ToastProvider in RouterProvider

--- src/routes/RouterProvider.jsx
+++ src/routes/RouterProvider.jsx
@@ -1,10 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, useHref } from 'react-router-dom';
 import { routes } from './routes';
 import { Layout } from '../components/Layout';
+import { HeroUIProvider } from '@heroui/react'
+import { ToastProvider } from "@heroui/toast";
 
 export const RouterProvider = () => {
+  const navigate = useNavigate();
+
   return (
     <BrowserRouter>
+      <HeroUIProvider navigate={navigate} useHref={useHref}>
+        <ToastProvider />
         <Routes>
           <Route element={<Layout />}>
             {routes.map(({ path, element: Element }) => (
@@ -16,6 +22,8 @@ export const RouterProvider = () => {
             ))}
           </Route>
         </Routes>
+      </HeroUIProvider>
+
     </BrowserRouter>
   );
 };
